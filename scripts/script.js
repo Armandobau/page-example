a02d4490d42ca4f3f816dd729a32c8fc8141ef6e
Armando Bautista <armandoloja92@gmail.com>
2016-08-23 10:15:16 +0200

    subida

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+//Menu despleglable
+function myFunction() {
+    document.getElementsByClassName("main-menu")[0].classList.toggle("responsive");
+}
+
+
+//Google Maps
+function initialize() {
+    
+    var myLatLng = {lat: 40.4308438, lng: -3.6628667};
+    var mapCanvas = document.getElementById('contact-map');
+    var mapOptions = {
+        center: new google.maps.LatLng(44.5403, -78.5463),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: myLatLng
+            
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'We are here!'
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
