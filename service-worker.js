712b19d98303c8809aa525d3680109b11ccb41f9
Armando Bautista <armandoloja92@gmail.com>
2017-03-21 08:38:31 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,2 @@
 console.log("hola");
+toolbox.precache(['/index.html', '/styles/styles.css', '/images/carpenterL.jpg', '/images/carpenterM.jpg', '/images/carpenterS.jpg', '/images/carpenterXS.jpg']);
