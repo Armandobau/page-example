a893c4fcbebed118f95d5d833d565d348ea57758
Armando Bautista <armandoloja92@gmail.com>
2017-03-22 10:58:07 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,26 @@
 console.log("hola");
-//toolbox.precache(['/index.html', '/styles/styles.css', '/images/carpenterL.jpg', '/images/carpenterM.jpg', '/images/carpenterS.jpg', '/images/carpenterXS.jpg']);
+
+var dataCacheName = 'ejemPrueba-v1';
+var cacheName = 'ejemplo-prueba-final-1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/script.js',
+  '/styles/styles.css',
+  '/images/profile.jpg',
+  '/images/profile2.jpg',
+  '/images/carpenterL.jpg',
+  '/images/carpenterM.jpg',
+  '/images/carpenterS.jpg',
+  '/images/carpenterXS.jpg'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
