c79f98cdcf370d507e0ccb4c4cb218c9ba48be83
Armando Bautista <armandoloja92@gmail.com>
2017-03-22 11:58:55 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,6 +12,7 @@ var filesToCache = [
   '/page-example/images/carpenterXS.jpg'
 ];
 
+
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
@@ -28,15 +29,17 @@ self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
+        if (key !== cacheName && key !== dataCacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
     })
   );
+  
   return self.clients.claim();
 });
+
 
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
