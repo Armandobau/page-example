5bdf52933381e79c38ad93191adaf7dc3526f572
Armando Bautista <armandoloja92@gmail.com>
2017-12-15 11:25:30 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
+/*
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
@@ -67,3 +68,86 @@ self.addEventListener('fetch', event => {
     );
   }
 });
+
+*/
+
+
+
+
+
+
+//This is the service worker with the Cache-first network
+
+var CACHE = 'pwabuilder-precache';
+var precacheFiles = [
+  '/',
+  '/page-example/',
+  '/page-example/index.html',
+  '/page-example/scripts/script.js',
+  '/page-example/styles/styles.css',
+  '/page-example/images/icon-144.png',
+  '/page-example/images/profile.jpeg',
+  '/page-example/images/profile2.jpeg',
+  '/page-example/images/carpenterL.jpg',
+  '/page-example/images/carpenterM.jpg',
+  '/page-example/images/carpenterS.jpg',
+  '/page-example/images/carpenterXS.jpg'
+    ];
+
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+  evt.waitUntil(precache().then(function() {
+    console.log('[ServiceWorker] Skip waiting on install');
+      return self.skipWaiting();
+
+  })
+  );
+});
+
+
+//allow sw to control of current page
+self.addEventListener('activate', function(event) {
+console.log('[ServiceWorker] Claiming clients for current page');
+      return self.clients.claim();
+
+});
+
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.'+ evt.request.url);
+  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  evt.waitUntil(update(evt.request));
+});
+
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(precacheFiles);
+  });
+}
+
+
+function fromCache(request) {
+  //we pull files from the cache first thing so we can show them fast
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+
+function update(request) {
+  //this is where we call the server to get the newest version of the 
+  //file to use the next time we show view
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
+function fromServer(request){
+  //this is the fallback if it is not in the cahche to go to the server and get it
+return fetch(request).then(function(response){ return response})
+}
